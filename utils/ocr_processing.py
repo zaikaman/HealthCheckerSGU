3fbc7a3405d453fc9a4e46ad67a9742b460a25d6
zaikaman <91252891+zaikaman@users.noreply.github.com>
2024-11-01 17:10:40 +0700

    Update ocr_processing.py

--- utils/ocr_processing.py
+++ utils/ocr_processing.py
@@ -1,8 +1,8 @@
+import os
 from PIL import Image
 import pytesseract
-import os
 
-os.environ["TESSDATA_PREFIX"] = "/usr/share/tesseract-ocr/5/tessdata/"
+os.environ["TESSDATA_PREFIX"] = "/app/tessdata/"
 
 def extract_text_from_image(filepath, lang='vie'):
     """Extract text from an image file using Tesseract OCR."""
