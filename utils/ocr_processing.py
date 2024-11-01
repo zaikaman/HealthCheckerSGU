c7f32f2f4d863b55185437fd807adf246150ac10
zaikaman <91252891+zaikaman@users.noreply.github.com>
2024-11-01 17:00:31 +0700

    Update ocr_processing.py

--- utils/ocr_processing.py
+++ utils/ocr_processing.py
@@ -1,11 +1,9 @@
 from PIL import Image
 import pytesseract
 
-# Đường dẫn tới tesseract.exe
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-
 def extract_text_from_image(filepath, lang='vie'):
     """Extract text from an image file using Tesseract OCR."""
     image = Image.open(filepath)
+    # Directly use pytesseract without specifying the Tesseract path
     text = pytesseract.image_to_string(image, lang=lang)
     return text
