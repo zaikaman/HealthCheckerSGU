038bcd24cf864ed467344cc076298556d63ba513
zaikaman <91252891+zaikaman@users.noreply.github.com>
2024-11-01 16:51:12 +0700

    Configured app for Heroku deployment

--- app.py
+++ app.py
@@ -39,4 +39,6 @@ def upload_file():
 if __name__ == '__main__':
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
-    app.run(debug=True)
+    # Bind to the Heroku-specified port
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
