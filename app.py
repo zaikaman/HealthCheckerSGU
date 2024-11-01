ebc0cdfaf7304675b25f1d82e11426183a98056e
zaikaman <91252891+zaikaman@users.noreply.github.com>
2024-11-01 22:12:35 +0700

    Added jsonify import

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import os
 from utils.ocr_processing import extract_text_from_image
