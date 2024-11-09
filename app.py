57ef340c4096354381450b9e9f9ed2304b6ab251
zaikaman <91252891+zaikaman@users.noreply.github.com>
2024-11-09 10:15:29 +0700

    Trying again 2

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = 'zaikaman123@gmail.com'  # Email của bạn
-app.config['MAIL_PASSWORD'] = 'UjLbSv7zrw2yGsgW'    # App password
+app.config['MAIL_PASSWORD'] = 'vgbz yfbt rahw bedt'    # App password
 
 # Khởi tạo Mail
 mail = Mail(app)
