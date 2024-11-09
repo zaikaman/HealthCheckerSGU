668eaf1a8e542834656fa7fb24aaf9805ba2943d
zaikaman <91252891+zaikaman@users.noreply.github.com>
2024-11-09 10:17:31 +0700

    Rollback

--- app.py
+++ app.py
@@ -85,12 +85,12 @@ cloudinary.config(
 )
 
 # Thêm cấu hình email sau app = Flask(__name__)
-app.config['MAIL_SERVER'] = 'smtp-relay.brevo.com'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_USERNAME'] = 'zaikaman123@gmail.com'  # Email của bạn
-app.config['MAIL_PASSWORD'] = 'vgbz yfbt rahw bedt'    # App password
+app.config['MAIL_USERNAME'] = 'thinhgpt1706@gmail.com'  # Email của bạn
+app.config['MAIL_PASSWORD'] = 'xgxn kjcv haqf sjxz'    # App password
 
 # Khởi tạo Mail
 mail = Mail(app)
