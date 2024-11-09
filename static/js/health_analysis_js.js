66898169404516eae12de0c8f47b75cf81272991
zaikaman <91252891+zaikaman@users.noreply.github.com>
2024-11-09 19:59:16 +0700

    Rollback

--- static/js/health_analysis_js.js
+++ static/js/health_analysis_js.js
@@ -58,16 +58,18 @@ function validateFile(file) {
     return true;
 }
 
-// Show file preview
+// Show file preview with animation
 function showFilePreview(file, container) {
     const reader = new FileReader();
     reader.onload = function(e) {
         container.style.opacity = '0';
         container.innerHTML = `
             <img src="${e.target.result}" class="img-preview img-fluid rounded" 
-                 style="max-height: 200px; margin-top: 10px;">
+                 alt="Preview" style="max-height: 300px;">
         `;
+        
         setTimeout(() => {
+            container.style.transition = 'opacity 0.5s ease';
             container.style.opacity = '1';
         }, 100);
     };
